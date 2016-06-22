8144c13f9a46f9233fe00a906540ce21548f4542
Darkhus1 <Darkhus1@hotmail.com>
2016-06-22 16:18:36 +0200

    Made A Hub To House More Games AND Added Game Content
    Game Content = FPS Slavia

---
+++ FPSSlavia/Hud.js
@@ -0,0 +1,17 @@
+function sethud(){
+			
+			document.getElementById('HUD').style.left ='0';
+			document.getElementById('HUD').style.top ='0';
+			
+			}
+			
+			sethud();
+			
+
+			var HUD = setInterval(function(){
+			
+		  
+		  
+			sethud();
+					
+			}, 1);
